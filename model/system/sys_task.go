740cb70d931d614115deca3c693bb4bca7a4c678
huruihao <1197289278@qq.com>
2025-02-26 11:45:21 +0800

    refactor(model): 移除 SysTask 结构体中 TaskType 字段的唯一约束
    移除了 SysTask 结构体中 TaskType 字段的唯一约束，保留了任务类型的注释。这个修改可能会影响任务类型的存储和查询，请确保在其他地方没有依赖这个唯一约束。

--- model/system/sys_task.go
+++ model/system/sys_task.go
@@ -4,7 +4,7 @@ type SysTask struct {
 	ProjectModel
 
 	Name     string `json:"name" gorm:"comment:任务名称"`
-	TaskType string `json:"taskType" gorm:"unique;comment:任务类型"`
+	TaskType string `json:"taskType" gorm:"comment:任务类型"`
 }
 
 func (t *SysTask) TableName() string {
